8dd4e133ffa115ff83b0e1b8ad2d173ea5e640d9
Patrick Prestes <patrickprestes977@gmail.com>
2024-07-22 10:42:08 -0300

    feat:creating feat add task

--- src/js/task.js
+++ src/js/task.js
@@ -3,3 +3,16 @@ const taskInput = document.querySelector('#new-task');
 const addTaskButton = document.querySelector('#add-task');
 const taskTemplate = document.querySelector('#tasj-template');
 
+addTaskButton.addEventListener('click', () =>{
+    const newTask = taskInput.value;
+
+    addTaskButton(newTask);
+});
+
+function addTask(task){
+      const newTask = {
+        id : Math.random().toString(16).slice(2),
+        text : task
+      }
+
+}
