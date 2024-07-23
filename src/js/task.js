5ab74317a008d543787386f033cce8736452e686
Patrick Prestes <patrickprestes977@gmail.com>
2024-07-23 10:45:12 -0300

    feat:Add and delete task functionality created

--- src/js/task.js
+++ src/js/task.js
@@ -1,24 +1,39 @@
-const taskOl = document.querySelector('#tasks');
-const taskInput = document.querySelector('#new-task');
-const addTaskButton = document.querySelector('#add-task');
-const taskTemplate = document.querySelector('#tasj-template');
+document.addEventListener('DOMContentLoaded', () => {
+    const taskOl = document.querySelector('#tasks');
+    const taskInput = document.querySelector('#new-task');
+    const addTaskButton = document.querySelector('#add-task');
+    const taskTemplate = document.querySelector('#task-template');
 
-addTaskButton.addEventListener('click', () =>{
-    const newTask = taskInput.value;
+    addTaskButton.addEventListener('click', () => {
+        const newTask = taskInput.value;
+        if (newTask) {
+            addTask(newTask);
+            taskInput.value = '';
+        }
+    });
 
-    addTaskButton(newTask);
+    function renderTask(newTask) {
+        const taskTemplateClone = taskTemplate.content.cloneNode(true);
+        const newTaskElement = taskTemplateClone.querySelector('.task');
+        const taskText = newTaskElement.querySelector('.task-text');
+        const deleteButton = newTaskElement.querySelector('.delete-button');
+
+        newTaskElement.id = newTask.id;
+        taskText.textContent = newTask.text;
+
+        deleteButton.addEventListener('click', () => {
+            taskOl.removeChild(newTaskElement);
+        });
+
+        taskOl.appendChild(taskTemplateClone);
+    }
+
+    function addTask(task) {
+        const newTask = {
+            id: Math.random().toString(16).slice(2),
+            text: task
+        };
+
+        renderTask(newTask);
+    }
 });
-
-function renderTask(newTask){
-    
-}
-
-
-function addTask(task){
-      const newTask = {
-        id : Math.random().toString(16).slice(2),
-        text : task
-      }
-    
-      renderTask(newTask);
-}
--- src/js/timer.js
+++ src/js/timer.js
@@ -1,78 +1,80 @@
-const pomodoroSelect = document.querySelector('#pomodoro');
-const shortBreakSelect = document.querySelector('#short-break');
-const longBreakSelect = document.querySelector('#long-break');
-const startButton = document.querySelector('#start');
-const timerParagraph = document.querySelector('#counter');
+document.addEventListener('DOMContentLoaded', () => {
+    const pomodoroSelect = document.querySelector('#pomodoro');
+    const shortBreakSelect = document.querySelector('#short-break');
+    const longBreakSelect = document.querySelector('#long-break');
+    const startButton = document.querySelector('#start');
+    const timerParagraph = document.querySelector('#counter');
 
-let selectedTimer = "pomodoro";
-let timerInterval;
+    let selectedTimer = "pomodoro";
+    let timerInterval;
 
-function changeSelectClasses(timer) {
-    if (timer === 'pomodoro') {
-        pomodoroSelect.classList.add("active-button");
-        shortBreakSelect.classList.remove("active-button");
-        longBreakSelect.classList.remove("active-button");
-    } else if (timer === 'short-break') {
-        pomodoroSelect.classList.remove("active-button");
-        shortBreakSelect.classList.add("active-button");
-        longBreakSelect.classList.remove("active-button");
-    } else if (timer === 'long-break') {
-        pomodoroSelect.classList.remove("active-button");
-        shortBreakSelect.classList.remove("active-button");
-        longBreakSelect.classList.add("active-button");
-    }
-}
-
-function secondsToMinutesSeconds(totalSeconds) {
-    const minutes = Math.floor(totalSeconds / 60);
-    const seconds = totalSeconds % 60;
-
-    const padSeconds = seconds.toString().padStart(2, "0");
-    return `${minutes}:${padSeconds}`;
-}
-
-function getTimerValue(timer) {
-    const timerValues = {
-        'pomodoro': 25 * 60,
-        'short-break': 5 * 60,
-        'long-break': 15 * 60,
-    };
-    return timerValues[timer];
-}
-
-function changeTimerValue(timer) {
-    const timerValueInSeconds = getTimerValue(timer);
-    timerParagraph.textContent = secondsToMinutesSeconds(timerValueInSeconds);
-}
-
-function selectTimer(timer) {
-    selectedTimer = timer;
-
-    changeSelectClasses(timer);
-    changeTimerValue(timer);
-}
-
-function startTimer(timer) {
-    let seconds = getTimerValue(timer);
-
-    if (timerInterval) {
-        clearInterval(timerInterval);
+    function changeSelectClasses(timer) {
+        if (timer === 'pomodoro') {
+            pomodoroSelect.classList.add("active-button");
+            shortBreakSelect.classList.remove("active-button");
+            longBreakSelect.classList.remove("active-button");
+        } else if (timer === 'short-break') {
+            pomodoroSelect.classList.remove("active-button");
+            shortBreakSelect.classList.add("active-button");
+            longBreakSelect.classList.remove("active-button");
+        } else if (timer === 'long-break') {
+            pomodoroSelect.classList.remove("active-button");
+            shortBreakSelect.classList.remove("active-button");
+            longBreakSelect.classList.add("active-button");
+        }
     }
 
-    timerInterval = setInterval(() => {
-        if (seconds > 0) {
-            seconds--;
-            timerParagraph.textContent = secondsToMinutesSeconds(seconds);
-        } else {
+    function secondsToMinutesSeconds(totalSeconds) {
+        const minutes = Math.floor(totalSeconds / 60);
+        const seconds = totalSeconds % 60;
+
+        const padSeconds = seconds.toString().padStart(2, "0");
+        return `${minutes}:${padSeconds}`;
+    }
+
+    function getTimerValue(timer) {
+        const timerValues = {
+            'pomodoro': 25 * 60,
+            'short-break': 5 * 60,
+            'long-break': 15 * 60,
+        };
+        return timerValues[timer];
+    }
+
+    function changeTimerValue(timer) {
+        const timerValueInSeconds = getTimerValue(timer);
+        timerParagraph.textContent = secondsToMinutesSeconds(timerValueInSeconds);
+    }
+
+    function selectTimer(timer) {
+        selectedTimer = timer;
+
+        changeSelectClasses(timer);
+        changeTimerValue(timer);
+    }
+
+    function startTimer(timer) {
+        let seconds = getTimerValue(timer);
+
+        if (timerInterval) {
             clearInterval(timerInterval);
         }
-    }, 1000);
-}
 
-pomodoroSelect.addEventListener('click', () => selectTimer('pomodoro'));
-shortBreakSelect.addEventListener('click', () => selectTimer('short-break'));
-longBreakSelect.addEventListener('click', () => selectTimer('long-break'));
+        timerInterval = setInterval(() => {
+            if (seconds > 0) {
+                seconds--;
+                timerParagraph.textContent = secondsToMinutesSeconds(seconds);
+            } else {
+                clearInterval(timerInterval);
+            }
+        }, 1000);
+    }
 
-startButton.addEventListener('click', () => startTimer(selectedTimer));
+    pomodoroSelect.addEventListener('click', () => selectTimer('pomodoro'));
+    shortBreakSelect.addEventListener('click', () => selectTimer('short-break'));
+    longBreakSelect.addEventListener('click', () => selectTimer('long-break'));
 
-changeTimerValue(selectedTimer);
+    startButton.addEventListener('click', () => startTimer(selectedTimer));
+
+    changeTimerValue(selectedTimer);
+});
